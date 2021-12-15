05c7b1c3ba5dfaf801b3da2b559a9f8f8b5780f5
devraj <devrajraushan49@gmail.com>
2021-12-15 07:57:31 -0800

    all file

---
+++ script.js
@@ -0,0 +1,83 @@
+var myresult=document.getElementById("result");
+
+function _one(){
+    var x=document.getElementById("one").value;
+    myresult.value+=x;
+}
+/******************Number Code Here ! */
+function _two(){
+    var x=document.getElementById("two").value;
+    myresult.value+=x;
+}
+function _three(){
+    var x=document.getElementById("three").value;
+    myresult.value+=x;
+}
+function _four(){
+    var x=document.getElementById("four").value;
+    myresult.value+=x;
+}
+function _five(){
+    var x=document.getElementById("five").value;
+    myresult.value+=x;
+}
+function _six(){
+    var x=document.getElementById("six").value;
+    myresult.value+=x;
+}
+function _sev(){
+    var x=document.getElementById("sev").value;
+    myresult.value+=x;
+}
+function _eigh(){
+    var x=document.getElementById("eigh").value;
+    myresult.value+=x;
+}
+function _nine(){
+    var x=document.getElementById("nine").value;
+    myresult.value+=x;
+}
+function _zero(){
+    var x=document.getElementById("zero").value;
+    myresult.value+=x;
+}
+/*****************Number Code Here !*** */
+/*****************************Symbol Code here !****** */
+
+function _dcm(){
+    var x=document.getElementById("dcm").value;
+    myresult.value+=x;
+}
+
+function _divd(){
+    var x=document.getElementById("divd").value;
+    myresult.value+=x;
+}
+
+function _subs(){
+    var x=document.getElementById("subs").value;
+    myresult.value+=x;
+}
+function _add(){
+    var x=document.getElementById("add").value;
+    myresult.value+=x;
+}
+function _mult(){
+    var x=document.getElementById("mult").value;
+    myresult.value+=x;
+
+}
+/******Symbol Code End Here !***** */
+
+/***********Answer Code here !****************** */
+function _ans(){
+    var x=document.getElementById("result");
+    var y=document.getElementById("result").value;
+    x.value=eval(y);
+
+}
+
+function _clear(){
+    myresult.value="";
+}
+//952563
